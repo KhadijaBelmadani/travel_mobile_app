237c0726aebdd4db3029d36820b1c7a1fd137e9e
hanane653 <hanane.amtdi@usmba.ac.ma>
2023-12-02 17:30:08 +0100

    thirdCommit

--- app/src/main/java/com/example/travel_app/first_UI.java
+++ app/src/main/java/com/example/travel_app/first_UI.java
@@ -2,13 +2,25 @@ package com.example.travel_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class first_UI extends AppCompatActivity {
+    Button start ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_ui);
+        Button start = findViewById(R.id.button);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(first_UI.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
