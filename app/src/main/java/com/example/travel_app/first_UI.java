9b18e612efb3bc7f49fbedf2019af2cb6128e48f
hanane653 <hanane.amtdi@usmba.ac.ma>
2023-12-02 15:53:53 +0100

    secondCommit

--- app/src/main/java/com/example/travel_app/first_UI.java
+++ app/src/main/java/com/example/travel_app/first_UI.java
@@ -4,15 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class first_UI extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first_ui);
     }
 }
-
-
-
-
