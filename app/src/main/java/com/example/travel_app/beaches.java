97af84b2fd21a585212660c3e094d4d77614b9f1
Khadija Belmadani <kbelmadani906@gmail.com>
2023-12-11 12:19:07 +0100

    third commit

---
+++ app/src/main/java/com/example/travel_app/beaches.java
@@ -0,0 +1,21 @@
+package com.example.travel_app;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class beaches extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beaches);
+
+    }
+
+
+}
